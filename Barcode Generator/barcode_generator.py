6f11cef2f628fac9c604940449b7b3736e8fe4a1
MIbonly76 <ilian_ivanov@abv.bg>
2023-10-07 16:48:20 +0300

    commit

--- Barcode Generator/barcode_generator.py
+++ Barcode Generator/barcode_generator.py
@@ -1,15 +1,13 @@
-import qrcode
+from barcode import EAN13
+from datetime import datetime
+file_name_id = str("My_QR_Code_") + datetime.now().strftime('%d%m%Y-%H%M%S')
 
-# Linked QR Code
+number = input("Enter The Number: ")
 
-qr = qrcode.QRCode(
-    version=1,
-    box_size=15,
-    border=15
-)
-
-data = input("Input your data: ")
-qr.add_data(data)
-qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
-img.save('Linked-Code.png')
+if not number.isdigit():
+    print("The entry value is not correct! It must be digits!")
+    exit(200)
+else:
+    number = int(number)
+    my_code = EAN13(number)
+    my_code.save(f"{file_name_id}.png")
